b309bf82250aa123a125546557b4b635dff17d2e
Belqisivana <belqisivana7@gmail.com>
2023-10-24 09:09:28 +0700

    commit push

---
+++ jobsheet 9/ArrayNilai07.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ArrayNilai07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       int [] NilaiAkhir = new int [10];
+       for (int i = 0; i < 10; i++) {
+        System.out.println("Masukkan nilai akhir ke- " + i +" : "); 
+        NilaiAkhir[i] = sc.nextInt();
+    }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Nilai akhir ke- " + i + "adalah " + NilaiAkhir[i]);
+        }
+    }
+    
+}
